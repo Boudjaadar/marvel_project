cd7764194d96be99fe6a7c6c061ba8d47fc1be61
boudjaadar <boudjaadar@yahoo.fr>
2025-03-12 01:46:41 +0100

    Ajout script push

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Vérifier si un message de commit est fourni
+if [ -z "$1" ]; then
+  echo "Veuillez entrer un message de commit."
+  exit 1
+fi
+
+# Ajouter tous les fichiers modifiés
+git add .
+
+# Faire un commit avec le message fourni
+git commit -m "$1"
+
+# Récupérer les dernières modifications du dépôt distant
+git pull origin main
